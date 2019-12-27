e7f7782456671025280080968ac64e778a0c57e3
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-12-27 13:12:23 +0100

    enhance the template

--- source/FifaResults/FifaResults.ConsoleApp/Program.cs
+++ source/FifaResults/FifaResults.ConsoleApp/Program.cs
@@ -12,18 +12,7 @@ namespace FifaResults.ConsoleApp
         {
             Console.WriteLine("Fifa 2019 Results");
 
-            Controller ctrl = new Controller();
-            ctrl.LoadClubs();
-            ctrl.LoadPlayersForClubs();
-
-            Console.WriteLine($"Es wurden {ctrl.Clubs.Count()} Clubs geladen.");
-
-            WriteContentToFile("top10clubs.md", ctrl.GetTop10ClubsAsMarkdown());
-            WriteContentToFile("last10clubs.md", ctrl.GetLast10ClubsAsMarkdown());
-            WriteContentToFile("players.md", ctrl.GetPlayersUnder100KValueByNationAndValueAsMarkdown());
-
-            Console.WriteLine();
-            Console.WriteLine("Beliebige Taste zum Beenden drücken.");
+            throw new NotImplementedException();
         }
 
         private static void WriteContentToFile(string fileName, string content)
